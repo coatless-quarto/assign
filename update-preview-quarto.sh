20607e3e70f84356a6b898a6b4e8404e3347285d
James J Balamuta <coatless@users.noreply.github.com>
2023-06-07 18:55:29 +0000

    Add a script to update the quarto version

---
+++ update-preview-quarto.sh
@@ -0,0 +1,5 @@
+wget -O quarto-latest.deb https://github.com/quarto-dev/quarto-cli/releases/download/v1.4.128/quarto-1.4.128-linux-amd64.deb
+
+sudo dpkg -i ./quarto-latest.deb
+
+rm quarto-latest.deb
