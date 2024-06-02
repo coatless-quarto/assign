e26f8d0225ee04934c945aeb9ba985cf92066b9e
James J Balamuta <james.balamuta@gmail.com>
2024-06-01 23:06:29 -0700

    Remove dev update script; add assign.code-workspace to ignore

--- update-preview-quarto.sh
+++
@@ -1,5 +0,0 @@
-wget -O quarto-latest.deb https://github.com/quarto-dev/quarto-cli/releases/download/v1.4.128/quarto-1.4.128-linux-amd64.deb
-
-sudo dpkg -i ./quarto-latest.deb
-
-rm quarto-latest.deb
