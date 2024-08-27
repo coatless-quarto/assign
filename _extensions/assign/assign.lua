5cf7903e44c9cf1cf11449c37e0eed7af5080563
James J Balamuta <james.balamuta@gmail.com>
2024-08-27 18:13:37 -0500

    Add Solution italics. Close #3

--- _extensions/assign/assign.lua
+++ _extensions/assign/assign.lua
@@ -28,6 +28,14 @@ function Div(div)
     table.insert(condition, {"when-profile", "rubric"})
   end
 
+  if div.classes:includes("sol") then 
+    -- Create a new Emph (italicized) element
+    local new_emph = pandoc.Emph{pandoc.Str("Solution.")}
+    
+    -- Add the new Emph element to the beginning of the Div's content
+    table.insert(div.content, 1, new_emph)
+  end
+
   -- Return the modified ConditionalBlock
   return quarto.ConditionalBlock({
     node = div, -- this is the div containing your content
